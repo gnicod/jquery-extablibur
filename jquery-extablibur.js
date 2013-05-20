98ee3dfc9c45863f7b58bb9b4e31a667a3f969c6
gnicod <nicod.ga@gmail.com>
2013-05-20 10:41:14 +0200

    add data-shrink option

--- jquery-extablibur.js
+++ jquery-extablibur.js
@@ -1,43 +1,55 @@
 (function($){
 	var defaults = {
-		column   : 1, //TODO what about multiple columns
+		toShrink : [],
 		callback : function(data,json){
+		}
+};
+
+function Extablibur(element,options){
+	this.options = $.extend( {}, defaults, options );
+	this._defaults = defaults;
+	this.table = element;
+	this._name = "extablibur";
+	this.init();		   
+}
+
+Extablibur.prototype = {
+	init : function(){
+			   _this = this;
+			   var _j = 0;
+			   this.table.find('th').each(function(){
+				   if($(this).attr('data-shrink')!==undefined){
+					   shval = $(this).attr('data-shrink');
+					   _this.options.toShrink.push([_j,shval]);
 				   }
-	};
+				   _j ++;
+			   });
 
-	function Extablibur(element,options){
-		this.options = $.extend( {}, defaults, options );
-		this._defaults = defaults;
-		this.table = element;
-		this._name = "extablibur";
-		this.init();		   
-	}
-
-	Extablibur.prototype = {
-		init : function(){
-				   _this = this;
-				   this.table.find("tbody tr").each(function(){
-					   console.log($(this).text());
-					   var td = $(($(this).find('td')[_this.options.column]));
+			   this.table.find("tbody tr").each(function(){
+				   for(var i=0;i<_this.options.toShrink;i++){
+					   var col   = i[0];
+					   var shval = i[1];
+					   var td = $(($(this).find('td')[col]));
 					   var fulltext = td.text();
-					   td.html(fulltext.substr(0,5)+"<span class='ext_longtd ext_longtd_h'>"+fulltext.substr(5,fulltext.length)+"</span>");
-				   });
-			   },
+					   td.html(fulltext.substr(0,shval)+"<span class='ext_longtd ext_longtd_h'>"+fulltext.substr(val,fulltext.length)+"</span>");
+				   }
+			   });
+		   },
 
-		expand : function(){
-					 this.table.find('.ext_longtd').removeClass('ext_longtd_h');
-				 },
+	expand : function(){
+				 this.table.find('.ext_longtd').removeClass('ext_longtd_h');
+			 },
 
-		shrink : function(){
-					 this.table.find('.ext_longtd').addClass('ext_longtd_h');
-				 }
-	};
+	shrink : function(){
+				 this.table.find('.ext_longtd').addClass('ext_longtd_h');
+			 }
+};
 
-	$.fn.extablibur = function (options) {
-		var ret = null;
-		this.each(function () {
-			ret = new Extablibur($(this), options);
-		});
-		return ret;
-	};
+$.fn.extablibur = function (options) {
+	var ret = null;
+	this.each(function () {
+		ret = new Extablibur($(this), options);
+	});
+	return ret;
+};
 }(window.jQuery));
